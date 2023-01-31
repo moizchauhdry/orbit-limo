b18795513aaff637551b7473604429d9d5ebc826
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-01-31 19:21:35 +0500

    # Squareup

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/online-booking', function () {
 });
 
 Route::get('/square-payment/{booking_id}', [PaymentController::class, 'squarePayment'])->name('booking.square-payment');
-Route::get('square-payment-success', [PaymentController::class, 'squarePaymentSuccess'])->name('booking.square-payment.success');
+Route::post('square-payment-success', [PaymentController::class, 'squarePaymentSuccess'])->name('booking.square-payment.success');
 Route::get('success', [PaymentController::class, 'success'])->name('success');
 
 
