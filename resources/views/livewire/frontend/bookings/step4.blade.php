a2e1c151a7ca26f7be2a17fba7aa397ff6d6b5a3
hamza khalil <hamzakhalil121@gmail.com>
2023-10-22 15:28:03 +0500

    Step 4 Booking

--- resources/views/livewire/frontend/bookings/step4.blade.php
+++ resources/views/livewire/frontend/bookings/step4.blade.php
@@ -5,7 +5,6 @@
             <div class="chbs-summary">
                 <div class="chbs-summary-header">
                     <h4>Contact &amp; Billing Info</h4>
-                    <a href="#" data-step="3">Edit</a>
                 </div>
                 <div class="chbs-summary-field">
                     <div class="chbs-layout-50x50 chbs-clear-fix">
@@ -30,7 +29,6 @@
             <div class="chbs-summary">
                 <div class="chbs-summary-header">
                     <h4>Ride details</h4>
-                    <a href="#" data-step="1">Edit</a>
                 </div>
                 @include('livewire.frontend.bookings.summary-ride-element')
             </div>
@@ -40,23 +38,22 @@
             <div class="chbs-summary">
                 <div class="chbs-summary-header">
                     <h4>Vehicle info</h4>
-                    <a href="#" data-step="2">Edit</a>
                 </div>
                 <div class="chbs-summary-field">
                     <div class="chbs-summary-field-name">Vehicle</div>
                     <div class="chbs-summary-field-value">{{getVehicle($vehicle_id)['name']}}</div>
                 </div>
             </div>
-            <div class="chbs-clear-fix chbs-coupon-code-section">
+            {{-- <div class="chbs-clear-fix chbs-coupon-code-section">
                 <div class="chbs-form-field">
                     <label>Do you have a discount code?</label>
-                    <input maxlength="12" wire:model="coupon_code" type="text">
+                    <input maxlength="12" name="chbs_coupon_code" value="" type="text">
                 </div>
-                <a href="#" class="chbs-button chbs-button-style-2" wire:click='applyCoupon'>
+                <a href="#" class="chbs-button chbs-button-style-2">
                     Apply code
                     <span class="chbs-meta-icon-arrow-horizontal"></span>
                 </a>
-            </div>
+            </div> --}}
             <div class="chbs-summary-price-element">
                 @include('livewire.frontend.bookings.summary-price-element')
             </div>
